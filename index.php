ecedd8c4911f7c0724c7bb18cdb67c2e1604a834
maruttoart <skip1122chibi1105@gmail.com>
2026-03-29 13:53:07 +0900

    youtube追加

--- index.php
+++ index.php
@@ -417,6 +417,10 @@ for ($i = 0; $i < 7; $i++) {
             fill: #E60023;
         }
 
+        .hero-social-link.youtube-link svg {
+            fill: #FF0000;
+        }
+
         /* セクション共通 */
         .section {
             padding: 60px 20px;
@@ -937,7 +941,7 @@ for ($i = 0; $i < 7; $i++) {
                     
                     <!-- SNSアイコン（PC版） -->
                     <div class="hero-social">
-                        <p class="hero-social-text">XやPinterestでも、新しいイラストをのんびり紹介しています</p>
+                        <p class="hero-social-text">X、Pinterest、YouTubeでも、新しいイラストをのんびり紹介しています</p>
                         <div class="hero-social-icons">
                             <a href="https://twitter.com/marutto_art" target="_blank" rel="noopener noreferrer" class="hero-social-link x-link" aria-label="X (Twitter)">
                                 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -947,6 +951,11 @@ for ($i = 0; $i < 7; $i++) {
                             <a href="https://www.pinterest.jp/maruttoart/" target="_blank" rel="noopener noreferrer" class="hero-social-link pinterest-link" aria-label="Pinterest">
                                 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M12 0C5.373 0 0 5.372 0 12c0 5.084 3.163 9.426 7.627 11.174-.105-.949-.2-2.405.042-3.441.218-.937 1.407-5.965 1.407-5.965s-.359-.719-.359-1.782c0-1.668.967-2.914 2.171-2.914 1.023 0 1.518.769 1.518 1.69 0 1.029-.655 2.568-.994 3.995-.283 1.194.599 2.169 1.777 2.169 2.133 0 3.772-2.249 3.772-5.495 0-2.873-2.064-4.882-5.012-4.882-3.414 0-5.418 2.561-5.418 5.207 0 1.031.397 2.138.893 2.738.098.119.112.224.083.345l-.333 1.36c-.053.22-.174.267-.402.161-1.499-.698-2.436-2.889-2.436-4.649 0-3.785 2.75-7.262 7.929-7.262 4.163 0 7.398 2.967 7.398 6.931 0 4.136-2.607 7.464-6.227 7.464-1.216 0-2.359-.631-2.75-1.378l-.748 2.853c-.271 1.043-1.002 2.35-1.492 3.146C9.57 23.812 10.763 24 12 24c6.627 0 12-5.373 12-12 0-6.628-5.373-12-12-12z"/>
+                                </svg>
+                            </a>
+                            <a href="https://www.youtube.com/@marutto_art" target="_blank" rel="noopener noreferrer" class="hero-social-link youtube-link" aria-label="YouTube">
+                                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
                                 </svg>
                             </a>
                         </div>
@@ -973,7 +982,7 @@ for ($i = 0; $i < 7; $i++) {
                     
                     <!-- SNSアイコン（モバイル版） -->
                     <div class="hero-social">
-                        <p class="hero-social-text">XやPinterestでも、新しいイラストをのんびり紹介しています</p>
+                        <p class="hero-social-text">X、Pinterest、YouTubeでも、新しいイラストをのんびり紹介しています</p>
                         <div class="hero-social-icons">
                             <a href="https://twitter.com/marutto_art" target="_blank" rel="noopener noreferrer" class="hero-social-link x-link" aria-label="X (Twitter)">
                                 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -983,6 +992,11 @@ for ($i = 0; $i < 7; $i++) {
                             <a href="https://www.pinterest.com/maruttoart/" target="_blank" rel="noopener noreferrer" class="hero-social-link pinterest-link" aria-label="Pinterest">
                                 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M12 0C5.373 0 0 5.372 0 12c0 5.084 3.163 9.426 7.627 11.174-.105-.949-.2-2.405.042-3.441.218-.937 1.407-5.965 1.407-5.965s-.359-.719-.359-1.782c0-1.668.967-2.914 2.171-2.914 1.023 0 1.518.769 1.518 1.69 0 1.029-.655 2.568-.994 3.995-.283 1.194.599 2.169 1.777 2.169 2.133 0 3.772-2.249 3.772-5.495 0-2.873-2.064-4.882-5.012-4.882-3.414 0-5.418 2.561-5.418 5.207 0 1.031.397 2.138.893 2.738.098.119.112.224.083.345l-.333 1.36c-.053.22-.174.267-.402.161-1.499-.698-2.436-2.889-2.436-4.649 0-3.785 2.75-7.262 7.929-7.262 4.163 0 7.398 2.967 7.398 6.931 0 4.136-2.607 7.464-6.227 7.464-1.216 0-2.359-.631-2.75-1.378l-.748 2.853c-.271 1.043-1.002 2.35-1.492 3.146C9.57 23.812 10.763 24 12 24c6.627 0 12-5.373 12-12 0-6.628-5.373-12-12-12z"/>
+                                </svg>
+                            </a>
+                            <a href="https://www.youtube.com/@marutto_art" target="_blank" rel="noopener noreferrer" class="hero-social-link youtube-link" aria-label="YouTube">
+                                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
                                 </svg>
                             </a>
                         </div>
